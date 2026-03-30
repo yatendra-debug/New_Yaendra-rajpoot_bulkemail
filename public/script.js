7f9ddb227c50c4b121698609d7a9144e4a207590
yatendra-debug <yatendra@besttoolsmail.online>
2026-03-30 21:55:21 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -11,6 +11,7 @@ async function send() {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
+        senderName: document.getElementById("name").value,
         email: document.getElementById("email").value,
         password: document.getElementById("pass").value,
         subject: document.getElementById("subject").value,
